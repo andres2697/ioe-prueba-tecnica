2c30f04d951a694a0acf48ec87c0b6451ecf44d7
andres2697 <75465089+andres2697@users.noreply.github.com>
2024-04-22 08:06:23 -0600

    Adding Spring Boot to project

---
+++ backend/src/main/java/com/ioepruebatecnica/ioepruebatecnica/IoePruebaTecnicaApplication.java
@@ -0,0 +1,13 @@
+package com.ioepruebatecnica.ioepruebatecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoePruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoePruebaTecnicaApplication.class, args);
+	}
+
+}
